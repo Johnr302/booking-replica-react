ba8b997dca80c22bbae71e71f98201a096c42bda
Johnr302 <johnathon.regalado@gmail.com>
2022-03-02 19:08:35 -0800

    Added more html

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ function App() {
     <div>
       <Header />
       <SearchLocation />
+      <div>
+        <input type="checkbox" id="travel" name="travel" />
+        <label for="travel">I'm traveling for work</label>
+      </div>
+      <section id="covidInfo">
+        <p>
+          Get the advice you need. Check latest COVID-19 restrictions before you
+          travel.
+          <a href="wg">Learn more</a>
+        </p>
+      </section>
+      <section>
+        <h1>Discover Early 2022 Deals</h1>
+        <p>Kick off your new-year travels with 15% off stays</p>
+        <button id="findDeals">Find deals</button>
+      </section>
     </div>
   );
 }
