03bbf07b85ba5c2da205e113d707cf3b9ca9fc57
Johnr302 <johnathon.regalado@gmail.com>
2022-03-08 20:50:38 -0800

    changes made to header html and css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,30 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div className="one">
-        <h1>Booking.com</h1>
-        <button> USD </button>
-        <button> Language </button>
-        <a href="" id="listProperty">
-          List your property
-        </a>
-        <a href="" id="register">
-          Register
-        </a>
-        <a href="" id="signIn">
-          Sign in
-        </a>
+      <div className="header-container">
+        <div className="inner-container">
+          <h1>Booking.com</h1>
+          <div>
+            <button id="currency">USD</button>
+            <button className="circle" id="language">
+              Language
+            </button>
+            <button className="circle" id="customerService">
+              ?
+            </button>
+            <a href="" id="listProperty">
+              List your property
+            </a>
+            <a href="" id="register">
+              Register
+            </a>
+            <a href="" id="signIn">
+              Sign in
+            </a>
+          </div>
+        </div>
       </div>
-      <div>
+      <div className="button-options-container">
         <button>Stays</button>
         <button>Flights</button>
         <button>Flight + Hotel</button>
