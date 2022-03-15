895adb93e4893a787d122f1df4d039fae6203ddc
Johnr302 <johnathon.regalado@gmail.com>
2022-03-14 19:39:28 -0700

    CSS and HTML changes for styling

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -3,13 +3,24 @@ import React from "react";
 const SearchLocation = () => {
   return (
     <div className="search-location">
-      <h1>Find your next stay</h1>
-      <p>Search deals on hotels, homes, and much more...</p>
+      <div className="search-location-text-container">
+        <h1>Find your next stay</h1>
+        <p>Search deals on hotels, homes, and much more...</p>
+      </div>
       <form>
-        <input type="text" placeholder="Where are you going?" />
-        <input type="date" />
-        <input placeholder="2 adults • 0 children • 1 room" />
-        <button type="submit"> Search </button>
+        <input
+          type="text"
+          className="search-form-item"
+          placeholder="Where are you going?"
+        />
+        <input type="date" className="search-form-item" />
+        <input
+          className="search-form-item"
+          placeholder="2 adults • 0 children • 1 room"
+        />
+        <button type="submit" className="search search-form-item">
+          Search
+        </button>
       </form>
     </div>
   );
