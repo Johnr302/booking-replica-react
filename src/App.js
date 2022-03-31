727b7ece9edde30ec04a9ca08ce13352827f6520
Johnr302 <johnathon.regalado@gmail.com>
2022-03-30 21:21:32 -0700

    Browse section and location section added html/css

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
           <h1>Explore United States of America</h1>
           <p>These popular destinations gave a lot to offer</p>
           <div className="explore-USA-card-container">
-            <div className="explore-card">
+            <div className="explore card">
               <img
                 src="https://cf.bstatic.com/xdata/images/city/square250/349720.webp?k=b7eca2cb90afd858f294732b0d5c194af47e70386b5be99373e3ff03be95c515&o="
                 alt="Las Vegas"
@@ -61,7 +61,7 @@ function App() {
               <h2>Las Vegas</h2>
               <p>565 properties</p>
             </div>
-            <div className="explore-card">
+            <div className="explore card">
               <img
                 src="https://cf.bstatic.com/xdata/images/city/square250/620103.webp?k=c07eb50383bfb4bedd11308fb59876bff7ef522dc379f4d96a48812cecaf45eb&o="
                 alt="Orlando"
@@ -69,7 +69,7 @@ function App() {
               <h2>Orlando</h2>
               <p>3,684 properties</p>
             </div>
-            <div className="explore-card">
+            <div className="explore card">
               <img
                 src="https://cf.bstatic.com/xdata/images/city/square250/690203.webp?k=dc8f5453ce80f38f94b1575299ae214a62239fd07eb6927c1518eeb703fa56c5&o="
                 alt="Myrtle Beach"
@@ -77,7 +77,7 @@ function App() {
               <h2>Myrtle Beach</h2>
               <p>2,642 properties</p>
             </div>
-            <div className="explore-card">
+            <div className="explore card">
               <img
                 src="https://cf.bstatic.com/xdata/images/city/square250/689603.webp?k=59e9972a1882387f6a8f35f6f2193c7f4e5203723651307392170ce756247bb7&o="
                 alt="Miami Beach"
@@ -85,7 +85,7 @@ function App() {
               <h2>Miami Beach</h2>
               <p>718 properties</p>
             </div>
-            <div className="explore-card">
+            <div className="explore card">
               <img
                 src="https://cf.bstatic.com/xdata/images/city/square250/689617.webp?k=edf88994b0c6b4c060300b942efdc1242289d1a695fcea13493e20596edc7daa&o="
                 alt="Panama City Beach"
@@ -93,7 +93,7 @@ function App() {
               <h2>Panama City Beach</h2>
               <p>2,077 properties</p>
             </div>
-            <div className="explore-card">
+            <div className="explore card">
               <img
                 src="https://cf.bstatic.com/xdata/images/city/square250/856691.webp?k=9cf21942b6aa8706b8721525de6a06b55e8980139ee21f53593982a8e988a55a&o="
                 alt="New York"
@@ -103,6 +103,85 @@ function App() {
             </div>
           </div>
         </section>
+        <section className="explore-USA browse">
+          <h1>Browse by property type</h1>
+          <div className="explore-USA-card-container browse">
+            <div className="browse card">
+              <img
+                src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
+                alt="Hotels"
+              />
+              <h2>Hotels</h2>
+              <p>848,827 hotels</p>
+            </div>
+            <div className="browse card">
+              <img
+                src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
+                alt="Apartment"
+              />
+              <h2>Apartment</h2>
+              <p>779,064</p>
+            </div>
+            <div className="browse card">
+              <img
+                src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
+                alt="Resorts"
+              />
+              <h2>Resorts</h2>
+              <p>17,632 resorts</p>
+            </div>
+            <div className="browse card">
+              <img
+                src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg"
+                alt="Villas"
+              />
+              <h2>Villas</h2>
+              <p>409,414 villas</p>
+            </div>
+            <div className="browse card">
+              <img
+                src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
+                alt="Cabins"
+              />
+              <h2>Cabins</h2>
+              <p>33.050 cabins</p>
+            </div>
+            <div className="browse card">
+              <img
+                src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_cottages/5e1fd9cd716f4825c6c7eac5abe692c52cc64516.jpg"
+                alt="Cottages"
+              />
+              <h2>Cottages</h2>
+              <p>137,810 cottages</p>
+            </div>
+          </div>
+        </section>
+        <div className="city-property-container">
+          <div className="major-location-container">
+            <div className="major-location" id="LasVegas">
+              <h1>Las Vegas</h1>
+              <p>566 properties</p>
+            </div>
+            <div className="major-location" id="SanDiego">
+              <h1>San Diego</h1>
+              <p>1,400 properties</p>
+            </div>
+          </div>
+          <div className="location-container">
+            <div className="location" id="SanFrancisco">
+              <h1>San Francisco</h1>
+              <p>377</p>
+            </div>
+            <div className="location" id="LosAngeles">
+              <h1>Los Angeles</h1>
+              <p>1,715 properties</p>
+            </div>
+            <div className="location" id="PalmSprings">
+              <h1>Palm Springs</h1>
+              <p>469 properties</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
