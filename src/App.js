88b34a3562f190ac5077ab251dbdf31220e12f5c
Johnr302 <johnathon.regalado@gmail.com>
2022-04-05 18:35:41 -0700

    html css for footer added logos

--- src/App.js
+++ src/App.js
@@ -213,53 +213,131 @@ function App() {
           </div>
           <div className="container-2">
             <ul>
-              <li>Countries</li>
-              <li>Regions</li>
-              <li>Cities</li>
-              <li>Districts</li>
-              <li>Airports</li>
-              <li>Hotels</li>
-              <li>Places of interest</li>
+              <li>
+                <a href="">Countries</a>
+              </li>
+              <li>
+                <a href="">Regions</a>
+              </li>
+              <li>
+                <a href="">Cities</a>
+              </li>
+              <li>
+                <a href="">Districts</a>
+              </li>
+              <li>
+                <a href="">Airports</a>
+              </li>
+              <li>
+                <a href="">Hotels</a>
+              </li>
+              <li>
+                <a href="">Places of interest</a>
+              </li>
             </ul>
             <ul>
-              <li>Homes</li>
-              <li>Apartments</li>
-              <li>Resorts</li>
-              <li>Villas</li>
-              <li>Hostels</li>
-              <li>B&Bs</li>
-              <li>Guest houses</li>
+              <li>
+                <a href="">Homes</a>
+              </li>
+              <li>
+                <a href="">Apartments</a>
+              </li>
+              <li>
+                <a href="">Resorts</a>
+              </li>
+              <li>
+                <a href="">Villas</a>
+              </li>
+              <li>
+                <a href="">Hostels</a>
+              </li>
+              <li>
+                <a href="">B&Bs</a>
+              </li>
+              <li>
+                <a href="">Guest houses</a>
+              </li>
             </ul>
             <ul>
-              <li>Unique places to stay</li>
-              <li>All destination</li>
-              <li>Discover</li>
-              <li>Reviews</li>
-              <li>Unpack: Travel articles</li>
-              <li>Season and holiday deals</li>
+              <li>
+                <a href="">Unique places to stay</a>
+              </li>
+              <li>
+                <a href="">All destination</a>
+              </li>
+              <li>
+                <a href="">Discover</a>
+              </li>
+              <li>
+                <a href="">Reviews</a>
+              </li>
+              <li>
+                <a href="">Unpack: Travel articles</a>
+              </li>
+              <li>
+                <a href="">Season and holiday deals</a>
+              </li>
             </ul>
             <ul>
-              <li>Car rental</li>
-              <li>Flight finder</li>
-              <li>Restaurant reservations</li>
-              <li>Booking.com for Travel Agents</li>
+              <li>
+                <a href="">Car rental</a>
+              </li>
+              <li>
+                <a href="">Flight finder</a>
+              </li>
+              <li>
+                <a href="">Restaurant reservations</a>
+              </li>
+              <li>
+                <a href="">Booking.com for Travel Agents</a>
+              </li>
             </ul>
             <ul>
-              <li>Coronavirus (COVID-19) FAQs</li>
-              <li>About Booking.com</li>
-              <li>Customer Service Help</li>
-              <li>Partner help</li>
-              <li>Careers</li>
-              <li>Sustainability</li>
-              <li>Press center</li>
-              <li>Safety Resource Center</li>
-              <li>Investor relations</li>
-              <li>Terms & conditions</li>
-              <li>Dispute resolution</li>
-              <li>How We Work</li>
-              <li>Privacy & cookie statement</li>
-              <li>Don't sell my personal information</li>
-              <li>Corporate contact</li>
+              <li>
+                <a href="">Coronavirus (COVID-19) FAQs</a>
+              </li>
+              <li>
+                <a href="">About Booking.com</a>
+              </li>
+              <li>
+                <a href="">Customer Service Help</a>
+              </li>
+              <li>
+                <a href="">Partner help</a>
+              </li>
+              <li>
+                <a href="">Careers</a>
+              </li>
+              <li>
+                <a href="">Sustainability</a>
+              </li>
+              <li>
+                <a href="">Press center</a>
+              </li>
+              <li>
+                <a href="">Safety Resource Center</a>
+              </li>
+              <li>
+                <a href="">Investor relations</a>
+              </li>
+              <li>
+                <a href="">Terms & conditions</a>
+              </li>
+              <li>
+                <a href="">Dispute resolution</a>
+              </li>
+              <li>
+                <a href="">How We Work</a>
+              </li>
+              <li>
+                <a href="">Privacy & cookie statement</a>
+              </li>
+              <li>
+                <a href="">Don't sell my personal information</a>
+              </li>
+              <li>
+                <a href="">Corporate contact</a>
+              </li>
             </ul>
           </div>
           <button>Extranet login</button>
@@ -268,7 +346,32 @@ function App() {
             Booking.com is part of Booking Holdings Inc., the world leader in
             online travel and related services.
           </p>
-          <div className="logo-container"></div>
+          <div className="logo-container">
+            <img
+              src="https://cf.bstatic.com/static/img/tfl/group_logos/logo_booking/27c8d1832de6a3123b6ee45b59ae2f81b0d9d0d0.png"
+              alt="Booking.com"
+            />
+            <img
+              src="https://cf.bstatic.com/static/img/tfl/group_logos/logo_priceline/f80e129541f2a952d470df2447373390f3dd4e44.png"
+              alt="priceline.com"
+            />
+            <img
+              src="https://cf.bstatic.com/static/img/tfl/group_logos/logo_kayak/83ef7122074473a6566094e957ff834badb58ce6.png"
+              alt="KAYAK"
+            />
+            <img
+              src="https://cf.bstatic.com/static/img/tfl/group_logos/logo_agoda/1c9191b6a3651bf030e41e99a153b64f449845ed.png"
+              alt="agoda"
+            />
+            <img
+              src="https://cf.bstatic.com/static/img/tfl/group_logos/logo_rentalcars/6bc5ec89d870111592a378bbe7a2086f0b01abc4.png"
+              alt="Rentacars.com"
+            />
+            <img
+              src="https://cf.bstatic.com/static/img/tfl/group_logos/logo_opentable/a4b50503eda6c15773d6e61c238230eb42fb050d.png"
+              alt="OpenTable"
+            />
+          </div>
         </footer>
       </div>
     </div>
